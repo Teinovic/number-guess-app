9093b390f297f77b6bc53bc95aaf25cf831d2ecb
Teinovic <momir.teinovic.2011@gmail.com>
2021-08-13 22:26:40 +0200

    adding basic styling to the starting screen

--- App.js
+++ App.js
@@ -1,11 +1,13 @@
 import React from 'react';
 import { StyleSheet, View } from 'react-native';
 import Header from './components/header';
+import StartGameScreen from './screens/gameStart';
 
 export default function App() {
   return (
     <View style={styles.screen}>
       <Header>Guess a number</Header>
+      <StartGameScreen>game screen</StartGameScreen>
     </View>
   );
 }
--- screens/gameStart.js
+++ screens/gameStart.js
@@ -1,14 +1,42 @@
 import React from 'react'
-import { View, StyleSheet, Text } from 'react-native'
+import { View, StyleSheet, Text, TextInput, Button } from 'react-native'
 
-export default function startGameScreen({children, ...restProps}) {
+export default function StartGameScreen({children, ...restProps}) {
     return (
-        <View>
-            <Text>{children}</Text>
+        <View style={style.screen}>
+            <Text style={style.title}>Start a new game!</Text>
+            <View style={style.inputContainer}>
+                <Text>Select a number</Text>
+                <TextInput />
+                <View style={style.buttonContainer}>
+                    <Button title='reset' />
+                    <Button title='confirm' />
+                </View>
+            </View>
         </View>
     )
 }
 
 const style = StyleSheet.create({
-    
+    screen: {
+        flex: 1,
+        padding: 10,
+        alignItems: 'center',
+        justifyContent: 'flex-start'
+    },
+    title: {
+        fontSize: 20,
+        marginVertical: 10
+    },
+    inputContainer: {
+        width: 300,
+        maxWidth: '80%',
+        alignItems: 'center'
+    },
+    buttonContainer: {
+        flexDirection: 'row',
+        justifyContent: 'space-between',
+        paddingHorizontal: 15,
+        width: '100%'
+    }
 })
